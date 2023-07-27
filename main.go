3f43acf372605392aa1972393a6970d3c2b791dc
Daniel <dap5@cin.ufpe.br>
2023-07-26 23:09:30 -0300

    Refactoring to use std http package

--- main.go
+++ main.go
@@ -1,83 +1,46 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/go-chi/chi"
-	"github.com/go-chi/cors"
-	"github.com/joho/godotenv"
 )
 
-func error_respond(writer http.ResponseWriter, code int, msg string) {
-	if code > 499 {
-		log.Println("Responding with 5XX error:", msg)
-	} else if code > 399 {
-		log.Println("Responding with 4XX error:", msg)
-	} else if code > 299 {
-		log.Println("Responding with 3XX error:", msg)
-	}
-	type errorResponse struct {
-		Error string `json:"error"`
-	}
-	json_respond(writer, code, errorResponse{
-		Error: msg,
-	})
-}
-func json_respond(writer http.ResponseWriter, code int, payload interface{}) {
-	data, err := json.Marshal(payload)
+func pingHandler(writer http.ResponseWriter, request *http.Request) {
+	log.Println("request received in /ping")
+	_, err := writer.Write([]byte("pong"))
 	if err != nil {
-		log.Printf("Failed to marshal JSON response: %v", payload)
-		writer.WriteHeader(500)
+		log.Println("Error when writing response")
+	}
+}
+
+func testHtmlTemplate(writer http.ResponseWriter, request *http.Request) {
+	tpl, err := template.ParseFiles("assets/test.gohtml")
+	if err != nil {
+		log.Println("Error when ..")
+	}
+	err = tpl.Execute(writer, struct {
+		Head string
+		Body string
+	}{
+		Head: "TEST",
+		Body: "test",
+	},
+	)
+	if err != nil {
 		return
 	}
-	writer.Header().Add("Content-Type", "application/json")
-	writer.WriteHeader(code)
-	writer.Write(data)
-}
-
-func handlreReadinnes(writer http.ResponseWriter, request *http.Request) {
-	json_respond(writer, 200, struct{}{})
-}
-
-func testErrorResponse(writer http.ResponseWriter, request *http.Request) {
-	error_respond(writer, 500, "Server error test")
 }
 
 func main() {
-	godotenv.Load()
 	fmt.Println("start")
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("Port not provided in configuration")
-	}
+	port := "8080"
 
-	router := chi.NewRouter()
-	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost"},
-		AllowedMethods:   []string{"http://localhost"},
-		AllowedHeaders:   []string{"*"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300,
-	}))
-
-	v1Router := chi.NewRouter()
-	v1Router.Get("/ping", handlreReadinnes)
-	v1Router.Get("/error", testErrorResponse)
-
-	router.Mount("/v1", v1Router)
-	server := &http.Server{
-		Handler: router,
-		Addr:    ":" + port,
-	}
-
-	log.Output(1, "Server starting at Port: "+port)
-	err := server.ListenAndServe()
+	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/test", testHtmlTemplate)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
-		log.Fatal("..")
+		log.Fatal("Server could not be started")
 	}
 }
